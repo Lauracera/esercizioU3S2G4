ec6bc48e52ea12544183cff9624175a27af1b401
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-30 16:13:16 +0100

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+    title = 'esercizio';
+}
